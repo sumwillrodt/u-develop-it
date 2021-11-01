9963ccd1a68c65d267f4ad7b3a41eff1468950a9
swillrodt <sumnerwillrodt@gmail.com>
2021-10-31 22:29:19 -0700

    connect mysql

--- server.js
+++ server.js
@@ -1,21 +1,35 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 //PORT designation and app expression
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Express middleware
+//Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-//test to confirm connection to Express.ks server
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World'
-    });
-});
+//Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: '',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
-// Default response for any other request (Not Found)
+//test to confirm connection to Express.ks server
+// app.get('/', (req, res) => {
+//     res.json({
+//         message: 'Hello World'
+//     });
+// });
+
+//Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
 });
